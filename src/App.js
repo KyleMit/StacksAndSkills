6d34ae303c3c0a29a9bdc5a200271494272280ae
KyleMit <kylemit@gmail.com>
2019-03-07 23:56:49 -0500

    add rest of data object scaffolding

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Stacks & Skills</h1>
+        <h1>Stacks & Skills <span className="text-muted">(v0.0.4)</span> </h1>
         <content className="Stacks">
           <Stack stacks={stackData}   />
         </content>
