2c97eb07e1c1dc988a756d9a27ec45aca81831af
Kyle Mitofsky <Kyle.Mitofsky@vermont.gov>
2019-07-16 09:29:47 -0400

    use package version in release version

--- README.md
+++ README.md
@@ -17,4 +17,5 @@ $ npm run build   # builds the app for production
 
 ## Resources
 
+* [Get version number from package.json in React Redux (create-react-app)](https://stackoverflow.com/a/50822003/1366033)
 * [ES Lint - Anchor is Valid](https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md)
--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Stacks & Skills <span className="text-muted">(v0.0.4)</span> </h1>
+        <h1>Stacks & Skills <span className="text-muted">(v{process.env.REACT_APP_VERSION})</span> </h1>
         <content className="Stacks">
           <Stack stacks={stackData}   />
         </content>
